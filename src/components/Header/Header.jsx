018fcb24b8e2f7ea39a4a718e203f22c2e3b7bb6
Vivi Ferliana Putri <tustika.nityasa@gmail.com>
2023-11-25 20:43:46 +0700

    add navbar & sidebar

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,24 +1,35 @@
-import React from 'react'
+import React, { useState } from 'react'
 import "./Header.css"
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faBars } from '@fortawesome/free-solid-svg-icons'
+import OutsideClickHandler from 'react-outside-click-handler'
 
 
 const Header = () => {
+  const [menuOpened, setMenuOpened] = useState(false);
+
+  const getMenuStyles=(menuOpened) =>{
+    if(document.documentElement.clientWidth<=800){
+      return {right: !menuOpened && "-100%"}
+    }
+  }
   return (
     <section className="h-wrapper">
-        <div className="ul flexCenter paddings innerWidth h-container">
+        <div className="flexCenter paddings innerWidth h-container">
             <a href="" className="navLogo">Tika</a>
 
-            <ul className="flexCenter h-menu">
+            <OutsideClickHandler
+            onOutsideClick={()=>{setMenuOpened(false)}}>
+            <ul className="flexCenter h-menu" style={getMenuStyles(menuOpened)}>
                 <li><a href="">Home</a></li>
                 <li><a href="">About</a></li>
                 <li><a href="">Skills</a></li>
                 <li><a href="">Portfolio</a></li>
                 <li><a href="">Contact</a></li>
             </ul>
+            </OutsideClickHandler>
 
-            <div className="menu-icon">
+            <div className="menu-icon" onClick={()=>setMenuOpened((prev)=>!prev)}>
             <FontAwesomeIcon icon={faBars} size="xl" />
             </div>
         </div>
