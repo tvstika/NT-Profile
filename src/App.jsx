f17a09a4b21fa1fb7cb1bd44cec53366cd6ca519
Vivi Ferliana Putri <tustika.nityasa@gmail.com>
2023-11-26 10:32:49 +0700

    add hero section

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import Header from "./components/Header/Header";
 import "./App.css";
+import Hero from "./components/Hero/Hero";
 
 function App() {
   return (
     <div className="App">
       <div>
         <Header/>
+        <Hero/>
       </div>
     </div>
   );
---
+++ src/components/Hero/Hero.jsx
@@ -0,0 +1,49 @@
+import React from 'react'
+import "./Hero.css"
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faLinkedin } from '@fortawesome/free-brands-svg-icons'
+import { faSquareGithub } from '@fortawesome/free-brands-svg-icons'
+import { faSquareBehance } from '@fortawesome/free-brands-svg-icons'
+
+const Hero = () => {
+  return (
+    <section className="hero-wrapper">
+        <div className="paddings innerwidth flexCenter hero-container">
+            {/* Left */}
+            <div className="flexColStart hero-left">
+                <a href="https://linkedin.com/in/nityasa-tustika-a63420291" className="social-icon">
+                <FontAwesomeIcon icon={faLinkedin} size="3x" />
+                </a>
+
+                <a href="https://github.com/tvstika" className="social-icon">
+                <FontAwesomeIcon icon={faSquareGithub} size="3x" />
+                </a>
+
+                <a href="https://www.behance.net/nityasatustika" className="social-icon">
+                <FontAwesomeIcon icon={faSquareBehance} size="3x" />
+                </a>
+            </div>
+
+            {/* Center */}
+            <div className="flexColStart hero-center">
+                <div className="blob"></div>
+            </div>
+
+            {/* Right */}
+            <div className="flexColStart hero-right">
+                <div className="hero-title">
+                    <h1 className='primaryText'>Nityasa Tustika</h1>
+                </div>
+                <div className="flexColStart hero-desc">
+                    <span>A computer science student with a strong interest <br /> in 
+                    website programming, UI/UX, graphic design, <br /> and also interested in game development.</span>
+                    <span>Recognized for my creativity, <br /> strong work ethic, and adaptability.</span>
+                </div>
+                
+            </div>
+        </div>
+    </section>
+  )
+}
+
+export default Hero
